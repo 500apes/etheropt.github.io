1cfbaea7ce21f7850247f7cb819b8655053c08a0
etheropt <frenchhoudini+etheropt@gmail.com>
2016-04-08 19:03:08 -0500

    update

--- config.js
+++ config.js
@@ -4,10 +4,10 @@ config.home_url = 'http://etheropt.github.io';
 // config.home_url = 'http://localhost:8080';
 config.contract_market = 'etheropt.sol';
 config.contract_addrs = [
-  '0x78798676a4b40b4650f9e8c07350a29953d5d933',
-  '0xb35abc849d143014f354486879bae428d696ab3b',
-  '0x0096800019bf4eca0bed5a714a553ecf844884c5',
   '0x73cbe96839b723bc913d10ba32b58fc476988a36',
+  '0x0096800019bf4eca0bed5a714a553ecf844884c5',
+  '0xb35abc849d143014f354486879bae428d696ab3b',
+  '0x78798676a4b40b4650f9e8c07350a29953d5d933',
   '0xa3d4d7df3988d48c48728787cb5910a8a4cc4d26',
 ];
 config.contract_addr = config.contract_addrs[0];
